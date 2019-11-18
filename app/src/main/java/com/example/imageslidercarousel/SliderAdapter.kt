44daf6d7b911e1703c0f83138470a04435c80530
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 21:27:23 -0500

    adapter

--- app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
+++ app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
@@ -1,4 +1,16 @@
 package com.example.imageslidercarousel
 
-class SliderAdapter {
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+class SliderAdapter : PagerAdapter {
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
