fef78bf29fcfc06f38a890b9f3dc6c3f36f494d2
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 21:52:02 -0500

    adapter

--- app/src/main/java/com/example/imageslidercarousel/MainActivity.kt
+++ app/src/main/java/com/example/imageslidercarousel/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.imageslidercarousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var images: Array<Int> = arrayOf(R.drawable.josue1, R.drawable.josue2, R.drawable.josue3)
+    var adapter:PagerAdapter = SliderAdapter(applicationContext,images)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewpager.adapter=adapter
     }
 }
