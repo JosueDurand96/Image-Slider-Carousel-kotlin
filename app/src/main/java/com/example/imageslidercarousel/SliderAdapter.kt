7f2b1837eaffadd7c6b828b4df37c8ffd88ee573
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 21:29:54 -0500

    adapter

--- app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
+++ app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
@@ -1,9 +1,18 @@
 package com.example.imageslidercarousel
 
+import android.content.Context
 import android.view.View
 import androidx.viewpager.widget.PagerAdapter
 
 class SliderAdapter : PagerAdapter {
+
+
+    var context: Context
+    var images: List<Int>
+
+    constructor(var context: Context, var images: Array<Int>) : super() {
+
+    }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
 
