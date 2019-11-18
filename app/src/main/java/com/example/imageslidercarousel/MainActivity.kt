ea6cb539e6d452451c623c06221e5d1793f88abe
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 21:50:41 -0500

    adapter

--- app/src/main/java/com/example/imageslidercarousel/MainActivity.kt
+++ app/src/main/java/com/example/imageslidercarousel/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var images: Array<Int> = arrayOf(R.drawable.josue1, R.drawable.josue2, R.drawable.josue3)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
