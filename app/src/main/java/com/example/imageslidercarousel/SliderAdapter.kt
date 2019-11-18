d499bd5e38f345eda5c22b19f0a7246bf73a2852
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 22:00:40 -0500

    adapter

--- app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
+++ app/src/main/java/com/example/imageslidercarousel/SliderAdapter.kt
@@ -6,11 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
-
 import androidx.viewpager.widget.PagerAdapter
-
 class SliderAdapter : PagerAdapter {
-
 
     var context: Context
     var images: Array<Int>
